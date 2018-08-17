f25454ce03c357fdfe3e56d842807bd624b65704
Jonathan Cole <coleji@bc.edu>
2018-08-17 09:46:29 -0400

    add build commands to readme, playing with struct/functions

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 rm ./dist -r
 mkdir dist
-cargo build --target wasm32-unknown-unknown
+cargo +nightly build --target wasm32-unknown-unknown
 wasm-bindgen target/wasm32-unknown-unknown/debug/spot_rust.wasm  --no-modules  --out-dir ./dist
--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,19 @@
 
 extern crate wasm_bindgen;
 use wasm_bindgen::prelude::*;
-
-#[wasm_bindgen]
-extern {
-    fn alert(s: &str);
+/*
+struct Board {
+    squares: Vec<Vec<String>>
 }
 
+fn parse_board(board_string: &str) -> Board {
+    let rows: Vec<&str> = board_string.split(":");
+    let squares = rows.into_iter().map(|row: &str | row.split("")).collect();
+    Board{
+        squares
+    }
+}
+*/
 #[wasm_bindgen]
 pub fn greet(name: &str) -> String {
     "Hello, ".to_owned() + name + "!"
