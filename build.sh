4170ed8885afd7cf4dfd2bd8a77dce1fabc5a3ac
Jonathan Cole <coleji@bc.edu>
2019-05-20 22:53:16 -0400

    dont use rust nightly, commit cargo lockfile

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 rm ./dist -r
 mkdir dist
-cargo +nightly build --target wasm32-unknown-unknown
+#cargo +nightly build --target wasm32-unknown-unknown
+cargo build --target wasm32-unknown-unknown
 wasm-bindgen target/wasm32-unknown-unknown/debug/spot_rust.wasm  --no-modules  --out-dir ./dist
